a3e9f57cbe9fcb0710b98c154dfd07b5a4e2d7ad
Keli Hansen <keli.p.hansen@gmail.com>
2018-05-30 21:40:54 -0700

    set up React basics

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/main.css';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
