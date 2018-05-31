6042d6de8e8a4359a4de1065642db6fa52b80daf
Keli Hansen <keli.p.hansen@gmail.com>
2018-05-30 23:52:42 -0700

    render list of items

---
+++ src/components/Item.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Item extends Component {
+  render() {
+    const { title, description, images } = this.props.item;
+
+    return (
+      <li>
+        <img src={images[0].n.url} alt={description}/>
+        <h3>{title}</h3>
+      </li>
+    );
+  }
+}
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+import Item from './Item';
+
+export default class Items extends Component {
+  render() {
+    const { items } = this.props;
+
+    return (
+      <ul>
+        {items.map((item, i) => <Item key={i} item={item}/>)}
+      </ul>
+    );
+  }
+}
