efe1ecdcccd8a2a5657a69d5d03ef86427667049
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 10:45:37 -0700

    touch up home

--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
     return (
       <div className={styles.home}>
         <p>I made an app!</p>
-        <p>It lets you search objects in the Smithsonian Design Museum&#8217;s collection by color.</p>
+        <p>It lets you search objects in the <a href="https://www.cooperhewitt.org/" target="_blank" rel="noopener noreferrer">Smithsonian Design Museum&#8217;s</a> collection by color.</p>
         <p>It&#8217;s a work in progress.</p>
       </div>
     );
