ec5766b36f7d8b209104cda2f6e660d3ce6e190b
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 10:39:32 -0700

    make home more homey

--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import styles from './Home.css';
 
 export default class Home extends Component {
   render() {
     return (
-      <h1>Home!</h1>
+      <div className={styles.home}>
+        <p>I made an app!</p>
+        <p>It lets you search objects in the Smithsonian Design Museum&#8217;s collection by color.</p>
+        <p>It&#8217;s a work in progress.</p>
+      </div>
     );
   }
 }
