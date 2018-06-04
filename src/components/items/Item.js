64c3d98b8df91d0be5cdf9e2d2fe72c5075555cd
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 12:52:10 -0700

    remove description

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -7,17 +7,16 @@ export default class Item extends Component {
   static propTypes = {
     id: PropTypes.string,
     title: PropTypes.string,
-    description: PropTypes.string,
     images: PropTypes.array
   };
 
   render() {
-    const { id, title, description, images } = this.props;
+    const { id, title, images } = this.props;
 
     return (
       <li>
         <Link to={`/items/${id}`}>
-          <img src={images[0].n.url} alt={description}/>
+          <img src={images[0].n.url} alt={title}/>
           <h3>{title}</h3>
         </Link>
       </li>
