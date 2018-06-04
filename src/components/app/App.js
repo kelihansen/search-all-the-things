05c2bf9d21894784e4967ffc6022c1cabf6fe3c4
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 00:04:44 -0700

    set up App with routes

---
+++ src/components/app/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Search from '../search/Search';
+import ItemDetail from '../items/ItemDetail';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header/>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/search" component={Search}/>
+              <Route path="/item/:id" render={({ match, history }) => {
+                return <ItemDetail objID={match.params.id} history={history}/>;
+              }}/>
+              <Redirect to="/"/>
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
