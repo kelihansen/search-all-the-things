064cb10e83e4db132a3375573d68b26fa47d9654
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 04:31:41 -0700

    modular css for header

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import styles from './Header.css';
+import styles from './Header.css';
 
 export default class Header extends Component {
   render() {
     return (
-      <header>
+      <header className={styles.header}>
         <h1>Explore the Cooper Hewitt Smithsonian Design Museum</h1>
         <nav>
           <ul>
