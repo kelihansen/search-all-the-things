c05883d67e1afa98d23d60a384163949565dcf85
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 12:51:07 -0700

    add get item by id

--- src/services/ch-api.js
+++ src/services/ch-api.js
@@ -1,11 +1,16 @@
 const ACCESS_TOKEN = ''; // <-- paste yours between the quotes
-const BASE_QUERY = `https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.objects&access_token=${ACCESS_TOKEN}`;
+const BASE_QUERY = 'https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.';
+const COLLECTION_QUERY = `${BASE_QUERY}search.objects&access_token=${ACCESS_TOKEN}`;
+const OBJECT_QUERY = `${BASE_QUERY}objects.getInfo&access_token=${ACCESS_TOKEN}`;
 
 const throwJson = json => { throw json; };
 const get = url => fetch(url)
   .then(r => r.ok ? r.json() : r.json().then(throwJson));
 
-export default function retrieve(color, page, perPage) {
-  return get(`${BASE_QUERY}&color=${color}&page=${page}&per_page=${perPage}`);
+export default function retrieve(color, page, perPage = 10) {
+  return get(`${COLLECTION_QUERY}&color=${color}&page=${page}&per_page=${perPage}`);
 }
 
+export function getItem(id) {
+  return get(`${OBJECT_QUERY}&object_id=${id}`);  
+}
