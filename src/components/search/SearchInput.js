f8f8907e1e075de0124d2f402d47bcf744c6c318
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 00:33:48 -0700

    move input to own module

---
+++ src/components/search/SearchInput.js
@@ -0,0 +1,46 @@
+/* eslint react/no-deprecated:off */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './SearchInput.css';
+
+export default class SearchInput extends Component {
+  static propTypes = {
+    searchTerm: PropTypes.string,
+    onSearch: PropTypes.func.isRequired
+  };
+
+  state = {
+    current: this.props.searchTerm || '#b8b8b8'
+  };
+
+  componentWillReceiveProps({ searchTerm }) {
+    if(searchTerm !== this.state.current) {
+      this.setState({ current: searchTerm || '#b8b8b8' });
+    }
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ current: target.value }, this.callSearch);
+  };
+
+  callSearch() {
+    this.props.onSearch(this.state);
+  }
+
+  render() {
+    const { current } = this.state;
+
+    return (
+      <section className="search-bar">
+        <label>
+        Select a color to search by:
+          <input
+            type="color"
+            value={current}
+            onChange={this.handleChange}  
+          />
+        </label>
+      </section>
+    );
+  }
+}
