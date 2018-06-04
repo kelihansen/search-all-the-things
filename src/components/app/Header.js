6faee15e2364f5eef53ba1ba6e8ef8799603fb3a
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-04 00:08:09 -0700

    set up Header as separate component

---
+++ src/components/app/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import styles from './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Explore the Cooper Hewitt Smithsonian Design Museum</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/search">Search</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
