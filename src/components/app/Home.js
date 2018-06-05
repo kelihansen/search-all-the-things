f5ed644a83b030fbaf9dddda69adde47cc1768c6
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-05 09:17:59 -0700

    add content

--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className={styles.home}>
-        <p>I made an app!</p>
+        <p>I made an app using React!</p>
         <p>It lets you search objects in the <a href="https://www.cooperhewitt.org/" target="_blank" rel="noopener noreferrer">Smithsonian Design Museum&#8217;s</a> collection by color.</p>
         <p>It&#8217;s a work in progress.</p>
       </div>
