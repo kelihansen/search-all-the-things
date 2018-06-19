28fda76c8d0881af8249886d01420f57223fbb1e
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-19 14:32:25 -0700

    user-loading reducer passes test

--- app/src/components/auth/reducers.js
+++ app/src/components/auth/reducers.js
@@ -1,5 +1,9 @@
+export const USER_AUTH = 'USER_AUTH';
+
 export function user(state = null, { type, payload }) {
   switch(type) {
+    case USER_AUTH:
+      return payload;
     default:
       return state;
   }
--- app/src/components/auth/reducers.test.js
+++ app/src/components/auth/reducers.test.js
@@ -1,10 +1,17 @@
 import {
-  user
+  user,
+  USER_AUTH
 } from './reducers';
 
 describe('user reducer', () => {
   it('has a default value of null', () => {
     const state = user(undefined, {});
     expect(state).toBe(null);
+  });
+
+  it('loads user', () => {
+    const info = { email: 'me@me.me', password:'111' };
+    const state = user(null, { type: USER_AUTH, payload: info });
+    expect(state).toEqual(info);
   });
 });
