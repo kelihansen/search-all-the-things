e36e3f7fbb1c654126ff3fd07e2c24b82c003f5e
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-18 19:47:10 -0700

    set up server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+const http = require('http');
+const app = require('./lib/app');
+const connect = require('./lib/util/connect');
+
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/cooper-hewitt';
+
+connect(MONGODB_URI);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log('server running on', server.address().port);
+});
