cd4f3925d6425515fe0bf9a32fd5bedd30da2962
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-18 23:23:50 -0700

    register exists plugin

--- server/lib/app.js
+++ server/lib/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const errorHandler = require('./util/error-handler');
 const ensureAuth = require('./auth/ensure-auth');
+require('./models/register-plugins');
 
 app.use(express.json());
 app.use(morgan('dev'));
@@ -11,7 +12,7 @@ const auth = require('./routes/auth');
 const items = require('./routes/items');
 
 app.use('/api/auth', auth);
-app.use('/api/items', ensureAuth, items);
+// app.use('/api/items', ensureAuth, items);
 
 app.use((req, res) => {
     res.sendFile('index.html', { root: './public' });
