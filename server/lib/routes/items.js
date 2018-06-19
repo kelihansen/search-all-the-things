069837f26b4bf8e2fc32d93517415c77ee407318
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-19 09:24:23 -0700

    well that seems better

--- server/lib/routes/items.js
+++ server/lib/routes/items.js
@@ -1,25 +1,19 @@
 const router = require('express').Router();
-const sa = require('superagent');
+const fetch = require('node-fetch');
 
 const CH_API_URL = 'https://api.collection.cooperhewitt.org/rest/';
 const CH_ACCESS_TOKEN = process.env.CH_ACCESS_TOKEN;
 
 module.exports = router
 
-    .get('/search-collection', (req, res, next) => {
+    .get('/find', (req, res, next) => {
         const { color, page } = req.query;
 
-        sa.get(CH_API_URL)
-            .query({
-                method: 'cooperhewitt.search.objects',
-                access_token: CH_ACCESS_TOKEN,
-                color: color,
-                page: page,
-                per_page: 10
-            })
-            .then(({ text }) => {
-                const parsed = JSON.parse(text);
-                const { objects, total } = parsed;
+        const searchUrl = `${CH_API_URL}?method=cooperhewitt.search.objects&access_token=${CH_ACCESS_TOKEN}&color=${color}&page=${page}&per_page=10`;
+
+        fetch(searchUrl)
+            .then(res => res.json())
+            .then(({ objects, total }) => {
                 const selected = objects.map(item => {
                     return { 
                         id: item.id,
@@ -34,6 +28,29 @@ module.exports = router
             })
             .catch(next);
     });
+
+// app.get('/search-location-weather', (req, res) => {
+//     //build api URL with user zip
+//     const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';	
+//     const apiId = '&appid=<YOUR API KEY GOES HERE>&units=imperial';
+
+//     const userLocation = (url1, url2, zipcode) => {
+//         let newUrl = url1 + zipcode + url2;
+//         return newUrl;
+//     };	
+
+//     const apiUrl = userLocation(baseUrl, apiId, zipcode);
+
+//     fetch(apiUrl)
+//     .then(res => res.json())
+//     .then(data => {
+//         res.send({ data });
+//     })
+//     .catch(err => {
+//         res.redirect('/error');
+//     });
+
+// })
 
 //     const ACCESS_TOKEN = ''; /* <-- paste yours between the quotes */
 // const BASE_QUERY = 'https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.';
