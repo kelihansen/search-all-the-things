9319e3de7580e5d1612c77d16f6f4612c9edeb3e
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-19 09:38:23 -0700

    fetching object by id works

--- server/lib/routes/items.js
+++ server/lib/routes/items.js
@@ -19,52 +19,27 @@ module.exports = router
                         id: item.id,
                         title: item.title,
                         smallImageUrl: item.images[0].n.url,
-                        largeImageUrl: item.images[0].z.url,
-                        medium: item.medium,
-                        description: item.description
                     };
                 });
-                res.send({ objects: selected, total });
+                res.send({ items: selected, total });
+            })
+            .catch(next);
+    })
+    
+    .get('/:id', (req, res, next) => {
+        const { id } = req.params;
+
+        const searchUrl = `${CH_API_URL}?method=cooperhewitt.objects.getInfo&access_token=${CH_ACCESS_TOKEN}&object_id=${id}`;
+
+        fetch(searchUrl)
+            .then(res => res.json())
+            .then(({ object }) => {
+                res.send({
+                    title: object.title,
+                    largeImageUrl: object.images[0].z.url,
+                    medium: object.medium,
+                    description: object.description
+                });
             })
             .catch(next);
     });
-
-// app.get('/search-location-weather', (req, res) => {
-//     //build api URL with user zip
-//     const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';	
-//     const apiId = '&appid=<YOUR API KEY GOES HERE>&units=imperial';
-
-//     const userLocation = (url1, url2, zipcode) => {
-//         let newUrl = url1 + zipcode + url2;
-//         return newUrl;
-//     };	
-
-//     const apiUrl = userLocation(baseUrl, apiId, zipcode);
-
-//     fetch(apiUrl)
-//     .then(res => res.json())
-//     .then(data => {
-//         res.send({ data });
-//     })
-//     .catch(err => {
-//         res.redirect('/error');
-//     });
-
-// })
-
-//     const ACCESS_TOKEN = ''; /* <-- paste yours between the quotes */
-// const BASE_QUERY = 'https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.';
-// const COLLECTION_QUERY = `${BASE_QUERY}search.objects&access_token=${ACCESS_TOKEN}`;
-// const OBJECT_QUERY = `${BASE_QUERY}objects.getInfo&access_token=${ACCESS_TOKEN}`;
-
-// const throwJson = json => { throw json; };
-// const get = url => fetch(url)
-//   .then(r => r.ok ? r.json() : r.json().then(throwJson));
-
-// export default function retrieve(color, page, perPage = 10) {
-//   return get(`${COLLECTION_QUERY}&color=${color}&page=${page}&per_page=${perPage}`);
-// }
-
-// export function getItem(id) {
-//   return get(`${OBJECT_QUERY}&object_id=${id}`);  
-// }
