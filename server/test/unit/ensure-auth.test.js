93697459d7c11edec5774584fc0fa1caa4b1e501
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-18 21:29:44 -0700

    ensure auth middleware passes basic test

---
+++ server/test/unit/ensure-auth.test.js
@@ -0,0 +1,25 @@
+const { assert } = require('chai');
+require('dotenv').config({ path: './test/.env' });
+const createEnsureAuth = require('../../lib/auth/ensure-auth');
+const tokenService = require('../../lib/auth/token-service');
+
+describe('ensure auth middleware', () => {
+    const user = { _id: '123' };
+    let token = '';
+    beforeEach(() => token = tokenService.sign(user));
+
+    const ensureAuth = createEnsureAuth();
+
+    it('adds payload as req.user on success', done => {
+        const req = {
+            get(header) {
+                if(header === 'Authorization') return token;
+            }
+        };
+        const next = () => {
+            assert.equal(req.user._id, user._id);
+            done();
+        };
+        ensureAuth(req, null, next);
+    });
+});
