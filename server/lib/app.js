c4371449c7a9a413f31d101d27428837a05524af
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-18 20:13:19 -0700

    set up app.js

---
+++ server/lib/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const errorHandler = require('./util/error-handler');
+const ensureAuth = require('./auth/ensure-auth');
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+const auth = require('./routes/auth');
+const items = require('./routes/pets');
+
+app.use('/api/auth', auth);
+app.use('/api/items', ensureAuth, items);
+
+app.use((req, res) => {
+    res.sendFile('index.html', { root: './public' });
+});
+
+app.use(errorHandler());
+
+module.exports = app;
