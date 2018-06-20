18acdf880af361ae4023070f83f41ba4b649c0d5
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-20 11:13:24 -0700

    add missing slash

--- app/src/components/app/PrivateRoute.js
+++ app/src/components/app/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getUser } from '..auth/reducers';
+import { getUser } from '../auth/reducers';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ user, component: Component, render, ...rest }) => {
