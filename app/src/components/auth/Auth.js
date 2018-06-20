750fe67172d90dffd19a2019952581a610d99831
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-20 10:17:28 -0700

    add auth component

---
+++ app/src/components/auth/Auth.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signin, signup } from './actions';
+import { getUser } from './reducers';
+import Credentials from './Credentials';
+
+class Auth extends PureComponent {
+  static propTypes = {
+    user: PropTypes.object,
+    signin: PropTypes.func.isRequired,
+    signup: PropTypes.func.isRequired,
+    location: PropTypes.object
+  };
+
+  render() {
+    const { user, signin, signup, location } = this.props;
+    const redirect = location.state ? location.state.from : '/';
+
+    if(user) return <Redirect to={redirect}/>;
+
+    return (
+      <section>
+        <Switch>
+          <Route path="/auth/signin" render={() => (
+            <div>
+              <h5>No account? <Link to="/auth/signup">sign up</Link></h5>
+              <Credentials action="SIGN IN" submit={signin}/>
+            </div>
+          )}/>
+          <Route path="/auth/signup" render={() => (
+            <div>
+              <h5>Have an account? <Link to="/auth/signup">sign up</Link></h5>
+              <Credentials action="SIGN UP" submit={signup} allowName={true}/>
+            </div>
+          )}/>
+          <Redirect to="/auth/signin"/>
+        </Switch>
+      </section>
+    );
+  }
+}
+
+export default connect(
+  state => ({ user: getUser(state) }),
+  { signup, signin }
+)(Auth);
