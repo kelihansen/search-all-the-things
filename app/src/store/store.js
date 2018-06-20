2298c9a3d61fb440c01767a4ca87085677889216
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-19 19:20:54 -0700

    update reducers in store

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,13 +1,19 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from './promise-middleware';
-import { loading, error } from '../components/app/reducers';
-import { user } from '../components/auth/reducers';
+import { loading, error, color } from '../components/app/reducers';
+import { user, checkedAuth } from '../components/auth/reducers';
+import { item, detailedItem, results } from '../components/search/reducers';
 
 const rootReducer = combineReducers({
   loading,
   error,
-  user
+  user,
+  checkedAuth,
+  color,
+  item,
+  detailedItem,
+  results
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
