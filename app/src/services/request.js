38d3e33d84e64e7adc9cc2b8b1ba6cc7b6fb1227
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-19 19:52:23 -0700

    follow Marty's example to subscribe to store

--- app/src/services/request.js
+++ app/src/services/request.js
@@ -1,6 +1,40 @@
+import store from '../store/store';
+import { getUser } from '../components/auth/reducers';
+
+let token = '';
+
+const key = 'user';
+const storage = window.localStorage;
+
+store.subscribe(() => {
+  const user = getUser(store.getState());
+  const retrievedToken = user ? (user.token || '') : '';
+  if(retrievedToken === token) return;
+
+  token = retrievedToken;
+  token ? storage.setItem(key, JSON.stringify(user)) : clearStoredUser();
+});
+
+export const clearStoredUser = () => storage.removeItem(key);
+
+export const getStoredUser = () => {
+  const json = storage.getItem(key);
+  try {
+    return JSON.parse(json);
+  }
+  catch(err) {
+    clearStoredUser();
+  }
+};
+
 function request(url, options = {}, data) {
   if(data) options.body = JSON.stringify(data);
   
+  if(token) {
+    if(!options.headers) options.headers = {};
+    options.headers.Authorization = token;
+  }
+
   return fetch(url, options)
     .then(response => [response.ok, response.json()])
     .then(([ok, json]) => {
