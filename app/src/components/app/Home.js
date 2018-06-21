fce347d2193516746336343f741c2a09d21a4687
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-20 20:12:39 -0700

    update home page

--- app/src/components/app/Home.js
+++ app/src/components/app/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import styles from './Home.css';
 
 export default class Home extends Component {
@@ -7,7 +7,6 @@ export default class Home extends Component {
       <div className={styles.home}>
         <p>I made an app using React!</p>
         <p>It lets you search objects in the <a href="https://www.cooperhewitt.org/" target="_blank" rel="noopener noreferrer">Smithsonian Design Museum&#8217;s</a> collection by color.</p>
-        <p>It&#8217;s a work in progress.</p>
       </div>
     );
   }
