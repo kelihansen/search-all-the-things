9a5f468291a2e7ef452cff4b0d0871dfe7d4fdc4
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-20 20:11:47 -0700

    add basic saved component

---
+++ app/src/components/saved/Saved.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Saved extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h3>Coming soon!</h3>
+      </div>
+    );
+  }
+}
+
+Saved.propTypes = {
+
+};
+
+export default Saved;
