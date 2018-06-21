32e393c6fba1166fb7c57075d0003d8f0e9d5a07
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-20 20:10:56 -0700

    add routes to app

--- app/src/components/app/App.js
+++ app/src/components/app/App.js
@@ -1,40 +1,57 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import Auth from '../auth/Auth';
 import Search from '../search/Search';
 import ItemDetail from '../items/ItemDetail';
+import PrivateRoute from './PrivateRoute';
+import Saved from '../saved/Saved';
+import { connect } from 'react-redux';
+import { getCheckedAuth } from '../auth/reducers';
+import { attemptUserLoad } from '../auth/actions';
 
-export default class App extends Component {
-  state = {
-    color: 'ffffff'
+class App extends PureComponent {
+  static propTypes = {
+    attemptUserLoad: PropTypes.func.isRequired,
+    checkedAuth: PropTypes.bool.isRequired
   };
 
-  handleColor = color => {
-    this.setState({ color });
-  };
+  componentDidMount() {
+    this.props.attemptUserLoad();
+  }
 
   render() {
-    const { color } = this.state;
+    const { checkedAuth } = this.props;
 
     return (
       <Router>
         <div>
-          <Header color={color}/>
+          <Header/>
           <main>
+            {checkedAuth &&
             <Switch>
               <Route exact path="/" component={Home}/>
+              <Route path="/auth" component={Auth}/>
               <Route path="/search" render={({ location, history }) => {
-                return <Search location={location} history={history} onColor={this.handleColor}/>;
+                return <Search location={location} history={history}/>;
               }}/>
               <Route path="/items/:id" render={({ match, history }) => {
                 return <ItemDetail objectID={match.params.id} history={history}/>;
               }}/>
+              <PrivateRoute path="/saved" component={Saved}/>
               <Redirect to="/"/>
             </Switch>
+            }
           </main>
         </div>
       </Router>
     );
   }
 }
+
+export default connect(
+  state => ({ checkedAuth: getCheckedAuth(state) }),
+  { attemptUserLoad }
+)(App);
