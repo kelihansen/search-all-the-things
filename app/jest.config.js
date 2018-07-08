4440a5a466d85523edaa3fd9873965028427fc72
Keli Hansen <keli.p.hansen@gmail.com>
2018-07-08 16:04:43 -0700

    rearrange for deployment

--- app/jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-module.exports = {
-  setupTestFrameworkScriptFile: '<rootDir>/enzyme.setup.js',
-  transform: {
-    '.*': '<rootDir>/node_modules/jest-css-modules'
-  }
-};
--- app/src/components/app/actions.test.js
+++
@@ -1,8 +0,0 @@
-import { COLOR_UPDATE } from './reducers';
-import { updateColor } from './actions';
-
-describe('color action creators', () => {
-  it('creates an update color action', () => {
-    expect(updateColor('123456')).toEqual({ type: COLOR_UPDATE, payload: '123456' });
-  });
-});
